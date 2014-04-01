bb4764b24072e38ebd2ebd2c1255879bed0e0674
Jingwen Owen Ou <jingweno@gmail.com>
2014-04-01 15:43:07 -0700

    Init of debug-proxy

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
+
+func ProxyServer(w http.ResponseWriter, req *http.Request) {
+	for k, v := range req.Header {
+		fmt.Printf("%s - %s\n", k, strings.Join(v, ","))
+	}
+
+	fmt.Fprintf(w, "hello, world!\n")
+}
+
+func main() {
+	http.HandleFunc("/", ProxyServer)
+	err := http.ListenAndServe(":12345", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
