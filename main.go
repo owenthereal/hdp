ad51d1b71ca993799e92b0505e3c57ab7d09ff92
Jingwen Owen Ou <jingweno@gmail.com>
2014-04-01 16:27:02 -0700

    Use colon to separate headers

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func ProxyServer(w http.ResponseWriter, req *http.Request) {
 	fmt.Printf("> %s %s %s\n", req.Method, reqPath(req), req.Proto)
 
 	for k, v := range req.Header {
-		fmt.Printf("> %s - %s\n", k, strings.Join(v, ","))
+		fmt.Printf("> %s: %s\n", k, strings.Join(v, ","))
 	}
 
 	content, err := ioutil.ReadAll(req.Body)
