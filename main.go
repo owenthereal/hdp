717bc183c67d88b18a69e5f95399fd12bd8a5d22
Jingwen Owen Ou <jingweno@gmail.com>
2014-04-01 16:22:28 -0700

    Better request path

--- main.go
+++ main.go
@@ -2,17 +2,44 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"strings"
 )
 
 func ProxyServer(w http.ResponseWriter, req *http.Request) {
+	req.Header.Set("Host", req.Host)
+
+	fmt.Printf("> %s %s %s\n", req.Method, reqPath(req), req.Proto)
+
 	for k, v := range req.Header {
-		fmt.Printf("%s - %s\n", k, strings.Join(v, ","))
+		fmt.Printf("> %s - %s\n", k, strings.Join(v, ","))
 	}
 
+	content, err := ioutil.ReadAll(req.Body)
+	req.Body.Close()
+	if err == nil {
+		fmt.Println(string(content))
+	}
+
+	fmt.Println("")
+
 	fmt.Fprintln(w, "ok")
+}
+
+func reqPath(req *http.Request) string {
+	path := req.URL.Path
+	query := []string{}
+	for k, v := range req.URL.Query() {
+		query = append(query, fmt.Sprintf("%s=%s", k, strings.Join(v, ",")))
+	}
+
+	if len(query) > 0 {
+		path = fmt.Sprintf("%s?%s", path, strings.Join(query, "&"))
+	}
+
+	return path
 }
 
 func main() {
