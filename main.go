f45ee8eadc86c8b69068c951611e2816b92b48c8
Jingwen Owen Ou <jingweno@gmail.com>
2014-04-02 10:53:59 -0700

    Add parsing for port

--- main.go
+++ main.go
@@ -1,13 +1,33 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
+var (
+	flagPort = flag.String("p", "12345", "port for http debug proxy")
+)
+
+func showUsage() {
+	fmt.Fprintf(os.Stderr,
+		"usage: %s [-p=<port>]\n",
+		os.Args[0])
+	fmt.Fprintf(os.Stderr,
+		"flags:\n")
+	flag.PrintDefaults()
+}
+
 func main() {
+	flag.Usage = showUsage
+	flag.Parse()
+
 	http.HandleFunc("/", Proxy)
-	err := http.ListenAndServe(":12345", nil)
+	fmt.Printf("HTTP Debug Proxy starting at %s...\n", *flagPort)
+	err := http.ListenAndServe(":"+*flagPort, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
