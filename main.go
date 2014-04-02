d818dad13c8ef3af411206c0ce80ce53ea52a64d
Jingwen Owen Ou <jingweno@gmail.com>
2014-04-02 08:25:59 -0700

    Extract out to proxy.go

--- main.go
+++ main.go
@@ -1,49 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
-	"strings"
 )
 
-func ProxyServer(w http.ResponseWriter, req *http.Request) {
-	req.Header.Set("Host", req.Host)
-
-	fmt.Printf("> %s %s %s\n", req.Method, reqPath(req), req.Proto)
-
-	for k, v := range req.Header {
-		fmt.Printf("> %s: %s\n", k, strings.Join(v, ","))
-	}
-
-	content, err := ioutil.ReadAll(req.Body)
-	req.Body.Close()
-	if err == nil {
-		fmt.Println(string(content))
-	}
-
-	fmt.Println("")
-
-	fmt.Fprintln(w, "ok")
-}
-
-func reqPath(req *http.Request) string {
-	path := req.URL.Path
-	query := []string{}
-	for k, v := range req.URL.Query() {
-		query = append(query, fmt.Sprintf("%s=%s", k, strings.Join(v, ",")))
-	}
-
-	if len(query) > 0 {
-		path = fmt.Sprintf("%s?%s", path, strings.Join(query, "&"))
-	}
-
-	return path
-}
-
 func main() {
-	http.HandleFunc("/", ProxyServer)
+	http.HandleFunc("/", Proxy)
 	err := http.ListenAndServe(":12345", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
---
+++ proxy.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
+func Proxy(w http.ResponseWriter, req *http.Request) {
+	// put back Host to the header
+	req.Header.Set("Host", req.Host)
+
+	displayReq(req)
+	displayHeader(req)
+	displayBody(req)
+	fmt.Println("")
+
+	fmt.Fprintln(w, "ok")
+}
+
+func displayReq(req *http.Request) {
+	display("%s %s %s\n", req.Method, reqPath(req), req.Proto)
+}
+
+func displayHeader(req *http.Request) {
+	for k, v := range req.Header {
+		display("%s: %s\n", k, strings.Join(v, ","))
+	}
+}
+
+func displayBody(req *http.Request) {
+	content, err := ioutil.ReadAll(req.Body)
+	defer req.Body.Close()
+
+	if err == nil {
+		fmt.Println(string(content))
+	}
+}
+
+func display(format string, a ...interface{}) {
+	format = fmt.Sprintf("> %s", format)
+	fmt.Printf(format, a...)
+}
+
+func reqPath(req *http.Request) string {
+	path := req.URL.Path
+	query := []string{}
+	for k, v := range req.URL.Query() {
+		query = append(query, fmt.Sprintf("%s=%s", k, strings.Join(v, ",")))
+	}
+
+	if len(query) > 0 {
+		path = fmt.Sprintf("%s?%s", path, strings.Join(query, "&"))
+	}
+
+	return path
+}
